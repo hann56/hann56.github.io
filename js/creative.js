266c41bf61e45696f5f15286c5017f4474c57044
tuandt56 <34866119+tuandt56@users.noreply.github.com>
2018-02-25 02:12:12 +0700

    têhd

---
+++ js/creative.js
@@ -0,0 +1,104 @@
+(function($) {
+  "use strict"; // Start of use strict
+
+  // Smooth scrolling using jQuery easing
+  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: (target.offset().top - 57)
+        }, 1000, "easeInOutExpo");
+        return false;
+      }
+    }
+  });
+
+  // Closes responsive menu when a scroll trigger link is clicked
+  $('.js-scroll-trigger').click(function() {
+    $('.navbar-collapse').collapse('hide');
+  });
+
+  // Activate scrollspy to add active class to navbar items on scroll
+  $('body').scrollspy({
+    target: '#mainNav',
+    offset: 57
+  });
+  <script>
+    // Thiết lập thời gian đích mà ta sẽ đếm
+    var countDownDate = new Date("Jan 5, 2018 15:37:25").getTime();
+
+    // cập nhập thời gian sau mỗi 1 giây
+    var x = setInterval(function() {
+
+      // Lấy thời gian hiện tại
+      var now = new Date().getTime();
+
+      // Lấy số thời gian chênh lệch
+      var distance = countDownDate - now;
+
+      // Tính toán số ngày, giờ, phút, giây từ thời gian chênh lệch
+      var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+      var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+      // HIển thị chuỗi thời gian trong thẻ p
+      document.getElementById("demo").innerHTML = days + "Ngày " + hours + "Giờ "
+      + minutes + "Phút " + seconds + "Giây ";
+
+      // Nếu thời gian kết thúc, hiển thị chuỗi thông báo
+      if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("demo").innerHTML = "Thời gian đếm ngược đã kết thúc";
+      }
+    }, 1000);
+  </script>
+  // Collapse Navbar
+  var navbarCollapse = function() {
+    if ($("#mainNav").offset().top > 100) {
+      $("#mainNav").addClass("navbar-shrink");
+    } else {
+      $("#mainNav").removeClass("navbar-shrink");
+    }
+  };
+  // Collapse now if page is not at top
+  navbarCollapse();
+  // Collapse the navbar when page is scrolled
+  $(window).scroll(navbarCollapse);
+
+  // Scroll reveal calls
+  window.sr = ScrollReveal();
+  sr.reveal('.sr-icons', {
+    duration: 600,
+    scale: 0.3,
+    distance: '0px'
+  }, 200);
+  sr.reveal('.sr-button', {
+    duration: 1000,
+    delay: 200
+  });
+  sr.reveal('.sr-contact', {
+    duration: 600,
+    scale: 0.3,
+    distance: '0px'
+  }, 300);
+
+  // Magnific popup calls
+  $('.popup-gallery').magnificPopup({
+    delegate: 'a',
+    type: 'image',
+    tLoading: 'Loading image #%curr%...',
+    mainClass: 'mfp-img-mobile',
+    gallery: {
+      enabled: true,
+      navigateByImgClick: true,
+      preload: [0, 1]
+    },
+    image: {
+      tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
+    }
+  });
+
+})(jQuery); // End of use strict
