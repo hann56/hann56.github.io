065a6e97e17ff1167eb372f104885b5a995891c5
Kayoko <kayoko96x@gmail.com>
2019-11-17 21:39:58 +0700

    update GUI

--- js/creative.js
+++ js/creative.js
@@ -25,36 +25,6 @@
     target: '#mainNav',
     offset: 57
   });
-  <script>
-    // Thiết lập thời gian đích mà ta sẽ đếm
-    var countDownDate = new Date("Jan 5, 2018 15:37:25").getTime();
-
-    // cập nhập thời gian sau mỗi 1 giây
-    var x = setInterval(function() {
-
-      // Lấy thời gian hiện tại
-      var now = new Date().getTime();
-
-      // Lấy số thời gian chênh lệch
-      var distance = countDownDate - now;
-
-      // Tính toán số ngày, giờ, phút, giây từ thời gian chênh lệch
-      var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-      var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-      var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-      var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-      // HIển thị chuỗi thời gian trong thẻ p
-      document.getElementById("demo").innerHTML = days + "Ngày " + hours + "Giờ "
-      + minutes + "Phút " + seconds + "Giây ";
-
-      // Nếu thời gian kết thúc, hiển thị chuỗi thông báo
-      if (distance < 0) {
-        clearInterval(x);
-        document.getElementById("demo").innerHTML = "Thời gian đếm ngược đã kết thúc";
-      }
-    }, 1000);
-  </script>
   // Collapse Navbar
   var navbarCollapse = function() {
     if ($("#mainNav").offset().top > 100) {
